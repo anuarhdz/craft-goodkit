3c4a28c7cc175899ad660ae3b08707e80955bb29
Anuar Reyes <anuar@hey.com>
2024-04-07 19:27:29 -0600

    add editorconfig and prettier php

--- config/general.php
+++ config/general.php
@@ -13,28 +13,28 @@ use craft\config\GeneralConfig;
 use craft\helpers\App;
 
 return GeneralConfig::create()
-  // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
-  ->defaultWeekStartDay(1)
-  // Prevent generated URLs from including "index.php"
-  ->omitScriptNameInUrls()
-  // Preload Single entries as Twig variables
-  ->preloadSingles(false)
-  // Prevent user enumeration attacks
-  ->preventUserEnumeration()
-  // Enable Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
-  ->devMode(App::env('DEV_MODE') ?? false)
-  // Allow administrative changes
-  ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES') ?? false)
-  // Disallow robots
-  ->disallowRobots(App::env('DISALLOW_ROBOTS') ?? false)
-  // Set the @webroot alias so the clear-caches command knows where to find CP resources
-  ->aliases([
-    '@webroot' => App::env('CRAFT_WEB_ROOT'),
-    '@web' => App::env('PRIMARY_SITE_URL'),
-    '@filesystemPath' => App::env('FILESYSTEM_PATH'),
-    '@filesystemUrl' => App::env('FILESYSTEM_URL'),
-    '@viteDevUrl' => App::env('VITE_DEV_URL'),
-    '@viteServerPublic' => rtrim(App::env('PRIMARY_SITE_URL'), '/') . '/dist/',
-    '@viteDevServerPublic' => rtrim(App::env('PRIMARY_SITE_URL'), '/') . ':5173',
-    '@assets' => '@webroot/assets',
-  ]);
+    // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
+    ->defaultWeekStartDay(1)
+    // Prevent generated URLs from including "index.php"
+    ->omitScriptNameInUrls()
+    // Preload Single entries as Twig variables
+    ->preloadSingles(false)
+    // Prevent user enumeration attacks
+    ->preventUserEnumeration()
+    // Enable Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+    ->devMode(App::env('DEV_MODE') ?? false)
+    // Allow administrative changes
+    ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES') ?? false)
+    // Disallow robots
+    ->disallowRobots(App::env('DISALLOW_ROBOTS') ?? false)
+    // Set the @webroot alias so the clear-caches command knows where to find CP resources
+    ->aliases([
+        '@webroot' => App::env('CRAFT_WEB_ROOT'),
+        '@web' => App::env('PRIMARY_SITE_URL'),
+        '@filesystemPath' => App::env('FILESYSTEM_PATH'),
+        '@filesystemUrl' => App::env('FILESYSTEM_URL'),
+        '@viteDevUrl' => App::env('VITE_DEV_URL'),
+        '@viteServerPublic' => rtrim(App::env('PRIMARY_SITE_URL'), '/') . '/dist/',
+        '@viteDevServerPublic' => rtrim(App::env('PRIMARY_SITE_URL'), '/') . ':5173',
+        '@assets' => '@webroot/assets',
+    ]);
--- config/vite.php
+++ config/vite.php
@@ -27,66 +27,66 @@ use craft\helpers\App;
 
 return [
 
-  /**
-   * @var bool Should the dev server be used?
-   */
-  'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
+    /**
+     * @var bool Should the dev server be used?
+     */
+    'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
 
-  /**
-   * @var string File system path (or URL) to the Vite-built manifest.json
-   */
-  'manifestPath' => '@webroot/dist/.vite/manifest.json',
+    /**
+     * @var string File system path (or URL) to the Vite-built manifest.json
+     */
+    'manifestPath' => '@webroot/dist/.vite/manifest.json',
 
-  /**
-   * @var string The public URL to the dev server (what appears in `<script src="">` tags
-   */
-  'devServerPublic' => '@viteDevServerPublic',
+    /**
+     * @var string The public URL to the dev server (what appears in `<script src="">` tags
+     */
+    'devServerPublic' => '@viteDevServerPublic',
 
-  /**
-   * @var string The public URL to use when not using the dev server
-   */
-  'serverPublic' => '@viteServerPublic',
+    /**
+     * @var string The public URL to use when not using the dev server
+     */
+    'serverPublic' => '@viteServerPublic',
 
-  /**
-   * @var string|array The JavaScript entry from the manifest.json to inject on Twig error pages
-   *              This can be a string or an array of strings
-   */
-  'errorEntry' => '',
+    /**
+     * @var string|array The JavaScript entry from the manifest.json to inject on Twig error pages
+     *              This can be a string or an array of strings
+     */
+    'errorEntry' => '',
 
-  /**
-   * @var string String to be appended to the cache key
-   */
-  'cacheKeySuffix' => '',
+    /**
+     * @var string String to be appended to the cache key
+     */
+    'cacheKeySuffix' => '',
 
-  /**
-   * @var string The internal URL to the dev server, when accessed from the environment in which PHP is executing
-   *              This can be the same as `$devServerPublic`, but may be different in containerized or VM setups.
-   *              ONLY used if $checkDevServer = true
-   */
-  'devServerInternal' => '@viteDevUrl',
+    /**
+     * @var string The internal URL to the dev server, when accessed from the environment in which PHP is executing
+     *              This can be the same as `$devServerPublic`, but may be different in containerized or VM setups.
+     *              ONLY used if $checkDevServer = true
+     */
+    'devServerInternal' => '@viteDevUrl',
 
-  /**
-   * @var bool Should we check for the presence of the dev server by pinging $devServerInternal to make sure it's running?
-   */
-  'checkDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
+    /**
+     * @var bool Should we check for the presence of the dev server by pinging $devServerInternal to make sure it's running?
+     */
+    'checkDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
 
-  /**
-   * @var bool Whether the react-refresh-shim should be included
-   */
-  'includeReactRefreshShim' => false,
+    /**
+     * @var bool Whether the react-refresh-shim should be included
+     */
+    'includeReactRefreshShim' => false,
 
-  /**
-   * @var bool Whether the modulepreload-polyfill shim should be included
-   */
-  'includeModulePreloadShim' => true,
+    /**
+     * @var bool Whether the modulepreload-polyfill shim should be included
+     */
+    'includeModulePreloadShim' => true,
 
-  /**
-   * @var string File system path (or URL) to where the Critical CSS files are stored
-   */
-  'criticalPath' => '',
+    /**
+     * @var string File system path (or URL) to where the Critical CSS files are stored
+     */
+    'criticalPath' => '',
 
-  /**
-   * @var string the suffix added to the name of the currently rendering template for the critical css file name
-   */
-  'criticalSuffix' => '',
+    /**
+     * @var string the suffix added to the name of the currently rendering template for the critical css file name
+     */
+    'criticalSuffix' => '',
 ];
