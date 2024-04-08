fa5479995558358a8583ee5085306c2924a8b3c6
Anuar Reyes <anuar@hey.com>
2024-04-08 08:14:54 -0600

    Update tailwind.config.js to include additional file extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js}", "./templates/**/*.{twig}"],
+  content: ['./src/**/*.js', './templates/**/*.{twig,html,php}'],
   theme: {
     extend: {},
   },
   plugins: [],
   corePlugins: {
     container: false,
-  }
-}
+  },
+};
